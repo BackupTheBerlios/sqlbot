3e041b52eb54d9c2e10c31f8a192ab769834a1c1
axllent <axllent>
2003-05-07 05:26:37 +0000

    Minor Changes

--- php-sqlbot/log-hub.php
+++ php-sqlbot/log-hub.php
@@ -2,9 +2,9 @@
 $page_title="Hub Log";
 include("header.ini");
 ?>
-
-
-
+<br>
+	<div align="center"><form action="log-hub.php" method="post">
+	<input class="button" type="Submit" value="Refresh"></form><br>
 <div align="center"><?
 $entry=0; 
 include("dbinfo.inc.php");
