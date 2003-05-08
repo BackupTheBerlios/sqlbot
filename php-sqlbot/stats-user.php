8610230eb0c83a5b7bd1fdcbfcf1bf99f8437efd
nutter <nutter>
2003-05-08 21:46:05 +0000

    Added user,client or IP search to this page

--- php-sqlbot/stats-user.php
+++ php-sqlbot/stats-user.php
@@ -4,22 +4,64 @@ include("header.ini");
 ?>
 
 <div align="center"><?
+$entry=0;
 $limit=$defaultLogEntries; 
 echo "$font";
 mysql_connect($databasehost,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 
+$wheresearch = "";
+$wherefilter = "";
+$and = 0;
 
-$numresults=mysql_query("SELECT * FROM user_stats ");
+if (!empty($nicksearch))  {
+	$wheresearch="name LIKE '%$nicksearch%'";
+	$ipsearch = "";
+	$clisearch = "";
+}
+else if (!empty($ipsearch)) {
+	$wheresearch="ip LIKE '%$ipsearch%'";
+	$nicksearch = "";
+	$clisearch = "";
+}
+else if (!empty($clisearch))  {
+	$wheresearch="client LIKE '%$clisearch%'";
+	$ipsearch = "";
+	$nicksearch = "";
+}
+if ((!empty($rfilter)) && (!empty($afilter))){
+	$wherefilter="reason='$rfilter' AND action='$afilter'";}
+else if (!empty($afilter))  {
+	$wherefilter="action='$afilter'";}
+else if (!empty($rfilter)){
+	$wherefilter="reason='$rfilter'";}
+
+if ((!empty($wheresearch)) && (!empty($wherefilter))){$where = "WHERE $wheresearch AND $wherefilter"; }
+else if (!empty($wheresearch)) {$where = "WHERE $wheresearch";}
+else if (!empty($wherefilter)) {$where = "WHERE $wherefilter";}
+else {$where = ""; }
+$numresults=mysql_query("SELECT * FROM user_stats $where");
 $numrows=mysql_num_rows($numresults);
 if (empty($offset)) {
 	$offset=0;}
 
-$result=mysql_query("SELECT * FROM user_stats ORDER by total_logins DESC  LIMIT $offset,$defaultLogEntries");
-
-//Say which entries from the log we are displaying
-
-echo "Total Number of Users $numrows<br>";
+$result=mysql_query("SELECT * FROM user_stats $where ORDER by total_logins DESC  LIMIT $offset,$defaultLogEntries");
+?>
+<b>Apply Filers</b>
+<table> 
+	<tr><td nowrap><form method="get" class='inline' action="stats-user.php">
+		Nick Search<input  TYPE="text" VALUE="<? echo "$nicksearch";?>" NAME="nicksearch" SIZE="30" MAXLENGTH="50" >
+		IP Search<input  TYPE="text" VALUE="<? echo "$ipsearch";?>" NAME="ipsearch" SIZE="20" MAXLENGTH="20" >
+		Client Search<input  TYPE="text" VALUE="<? echo "$clisearch";?>" NAME="clisearch" SIZE="20" MAXLENGTH="20" >
+	</td>
+	<td nowrap>
+		<tr><input type="submit" value="Apply"></tr>
+	</td></tr>
+</form>
+</td>
+</table>		
+<?
+echo "Totals :Users $numrows, Share TODO [GB]<br>";
 ?>
 <table border="$tableborders" cellspacing="2" cellpadding="2">
 <tr>
@@ -65,7 +107,7 @@ echo "</table>";
 
 if ($offset!=0) { 
     $prevoffset=$offset-$defaultLogEntries;
-    print "<a href=\"?m=$menuname&p=$path/userstats&offset=$prevoffset\">PREV</a> &nbsp; \n";
+    print "<a href=\"?m=$menuname&p=$path/userstats&offset=$prevoffset&offset=$offset&ipsearch=$ipsearch&clisearch=$clisearch&nicksearch=$nicksearch\">PREV</a> &nbsp; \n";
 }
 $pages=intval($numrows/$limit);
 
@@ -74,12 +116,12 @@ if ($numrows%$limit) {
 
 for ($i=1;$i<=$pages;$i++) { // loop thru
     $newoffset=$limit*($i-1);
-    print "<a href=\"?m=$menuname&p=$path/userstats&offset=$newoffset\">$i</a> &nbsp; \n"; }
+    print "<a href=\"?m=$menuname&p=$path/userstats&offset=$newoffset&offset=$offset&ipsearch=$ipsearch&clisearch=$clisearch&nicksearch=$nicksearch\">$i</a> &nbsp; \n"; }
 
 if (!(($offset/$limit)==$pages-1) && $pages!=1) {
     // not last page so give NEXT link
     $newoffset=$offset+$limit;
-    print "<a href=\"?m=$menuname&p=$path/userstats&offset=$newoffset\">NEXT</a><p>\n";
+    print "<a href=\"?m=$menuname&p=$path/userstats&offset=$newoffset&offset=$offset&ipsearch=$ipsearch&clisearch=$clisearch&nicksearch=$nicksearch\">NEXT</a><p>\n";
 }
 mysql_close();
 ?></div>
