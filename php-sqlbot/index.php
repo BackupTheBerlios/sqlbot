bbf1581759af093c4c8c7f57ca1dbe2c67bef251
axllent <axllent>
2003-05-08 15:44:53 +0000

    Spelling errors :-( Design layout change

--- php-sqlbot/index.php
+++ php-sqlbot/index.php
@@ -3,11 +3,11 @@ $page_title="SqlBot PHP Configurator";
 include("header.ini");
 ?>
 
-<div align="center"><table width="500" class="intro">
+<div align="center"><table width="600" class="intro">
 <tr>
 <td>Welcome to the configuration utility for SqlBot. Here you can easily configure and view your logs via the buttons above.<p>
 
-Please keep in mind that this project is still in the early staged, and so some things may not work properly. We are working hard to add new features, and fix old bugs.
+Please keep in mind that this project is still in the early stages, and so some things may not work properly. We are working hard to add new features, and fix old bugs.
 <div align="right"><em>... the sqlbot team</em></div></td>
 </tr>
 </table></div>
