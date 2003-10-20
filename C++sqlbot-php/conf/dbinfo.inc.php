fb54c0f2459ed5b20c2f65daa533bd7d044c8f7b
axllent <axllent>
2003-10-20 13:34:55 +0000

    Slight Modifications

---
+++ C++sqlbot-php/conf/dbinfo.inc.php
@@ -0,0 +1,17 @@
+<?
+// Host of MySQL DB
+$databasehost="localhost";
+
+// MySQL DB login
+$username="username";
+$password="password";
+
+//Name of DB
+$database="sqlbot";
+
+// Name of the Hub (Used only for headers)
+$hubname="";
+
+//The Default number of entries to display, commands like !log
+$defaultLogEntries = 20;
+?> 
