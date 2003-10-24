2ad8f1828985c90357e7f6651daaf67f3700e560
axllent <axllent>
2003-10-24 18:52:02 +0000

    Minor changes

--- C++sqlbot-php/conf/udbmenu.php
+++ C++sqlbot-php/conf/udbmenu.php
@@ -2,19 +2,19 @@
 	<LEGEND><font color="#FFFFFF">User DB</font></LEGEND>
 		<form action="<?php echo "userdb.php"; ?>" method="post">
 		<?php hidden_value(hubID, $hubID); ?>
-		<?php hidden_value(parse, All); ?>
-		<?php hidden_value(parseorder, uiNick); ?>
-		<input type="submit" value="All" class="menubutton"></form>
-	
-		<form action="<?php echo "userdb.php"; ?>" method="post">
-		<?php hidden_value(hubID, $hubID); ?>
 		<?php hidden_value(parse, Online); ?>
 		<?php hidden_value(parseorder, uiNick); ?>
 		<input type="submit" value="Online" class="menubutton"></form>
-		
+
 		<form action="<?php echo "userdb.php"; ?>" method="post">
+		<?php hidden_value(hubID, $hubID); ?>
+		<?php hidden_value(parse, All); ?>
+		<?php hidden_value(parseorder, uiNick); ?>
+		<input type="submit" value="All" class="menubutton"></form>
+
+		<!-- <form action="<?php echo "userdb.php"; ?>" method="post">
 		<?php hidden_value(hubID, $hubID); ?>
 		<?php hidden_value(parse, Fakers); ?>
 		<?php hidden_value(parseorder, uiNick); ?>
-		<input type="submit" value="Fakers" class="menubutton"></form>
+		<input type="submit" value="Fakers" class="menubutton"></form>-->
 </FIELDSET>
