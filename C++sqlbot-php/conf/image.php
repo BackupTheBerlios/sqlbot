7469c6a3e4a1b2c8e9021add813f7596d1e90f35
axllent <axllent>
2004-02-21 00:24:00 +0000

    *** empty log message ***

--- C++sqlbot-php/conf/image.php
+++
@@ -1,7 +0,0 @@
-<?php header ("Content-type: image/png");
-import_request_variables ( "gp" );
-$image = imagecreate($w,10);
-$gold = imagecolorallocate($image, 8, 3, 158);
-imagepng($image);
-imagedestroy($image);
-?>
