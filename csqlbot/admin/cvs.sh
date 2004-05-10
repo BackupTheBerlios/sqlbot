9e3af930466e189288e30fb1c25eb4c5ec82fcca
mathen <mathen>
2004-05-10 18:29:49 +0000

    - update

--- csqlbot/admin/cvs.sh
+++ csqlbot/admin/cvs.sh
@@ -64,7 +64,7 @@ case $AUTOMAKE_STRING in
     echo "*** KDE requires automake 1.5"
     exit 1
     ;;
-  automake*1.4* | automake*1.5* | automake*1.5-* | automake*1.6.* | automake*1.7* ) : ;;
+  automake*1.4* | automake*1.5* | automake*1.5-* | automake*1.6.* | automake*1.7* | automake*1.8* ) : ;;
   "" )
     echo "*** AUTOMAKE NOT FOUND!."
     echo "*** KDE requires automake 1.5"
